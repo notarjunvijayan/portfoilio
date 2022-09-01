031be7c3b54d2f06a845406d49f7a5eab4d59018
notarjunvijayan <u2004019@rajagiri.edu.in>
2022-09-01 13:21:07 +0530

    script fix

--- script.sh
+++ script.sh
@@ -3,9 +3,10 @@
 set -o errexit
 
 poetry install
+pip install setuptools
 pip install render
 pip install django
-pip install setuptools
+
 
 python manage.py collectstatic --no-input
 python manage.py migrate
