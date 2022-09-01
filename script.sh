aedda01e3d3981004462549729529d3b8c448cce
notarjunvijayan <u2004019@rajagiri.edu.in>
2022-09-01 12:48:42 +0530

    added pip

--- script.sh
+++ script.sh
@@ -3,8 +3,6 @@
 set -o errexit
 
 poetry install
-pip install django
-poetry add gunicorn
-
-python manage.py collectstatic --no-input
-python manage.py migrate
+pip install setuptools
+python3 manage.py collectstatic --no-input
+python3 manage.py migrate
