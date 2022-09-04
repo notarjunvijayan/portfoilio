0d5f6625defe0e94652e2f1ebe07424d39e53851
notarjunvijayan <u2004019@rajagiri.edu.in>
2022-09-04 12:30:06 +0530

    template canges

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -55,11 +55,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'portfolio.urls'
-
+TEMPLATES_DIR = os.path.join(BASE_DIR,'templates'),
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
